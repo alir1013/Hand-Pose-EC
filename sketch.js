91036de70b7fd7bd8a6fbfa736b5da701103463c
Roan Ali <alir13@usf.edu>
2024-11-25 17:51:48 -0500

    Final Product

--- sketch.js
+++ sketch.js
@@ -1,32 +1,25 @@
-//Hand-Pose Extra Credit
-let handPose;
+//Hand-Pose EC
 
+let handPose;
 function preload() {
     handPose = ml5.handPose();
   }
   
   let video;
-
   function setup() {
     createCanvas(640, 480);
-
-    // Create the video and hide it
-    video = createCapture(VIDEO);
-    video.size(640, 480);
-    video.hide();
-  }
   
-  let hands = [];
-
-  function setup() {
-    // ...
-    video.hide();
-  
+  // Create the video and hide it
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  video.hide();
     // Start detecting hands from the webcam video
-    handPose.detectStart(video, gotHands);
-  }
-  
-  // Callback function for when handPose outputs data
+  handPose.detectStart(video, gotHands);
+}
+
+let hands = [];
+
+   // Callback function for when handPose outputs data
 function gotHands(results) {
     // Save the output to the hands variable
     hands = results;
@@ -35,13 +28,12 @@ function gotHands(results) {
   function draw() {
     image(video, 0, 0, width, height);
   
-      // Draw all the tracked hand points
-  for (let i = 0; i < hands.length; i++) {
-    let hand = hands[i];
-
-    for (let j = 0; j < hand.keypoints.length; j++) {
-        let keypoint = hand.keypoints[j];
-        fill(0, 255, 0);
+    // Draw all the tracked hand points
+    for (let i = 0; i < hands.length; i++) {
+        let hand = hands[i];
+        for (let j = 0; j < hand.keypoints.length; j++) {
+            let keypoint = hand.keypoints[j];
+            fill(0, 255, 0);
       noStroke();
       circle(keypoint.x, keypoint.y, 10);
     }
